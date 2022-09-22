f2c2026147f26becbc096d7ceafa0e0ee5af8490
Mai Abdelhameed <maiabdelhameed16@gmail.com>
2022-09-22 20:48:21 +0200

    reactapp

---
+++ reactwebapi/src/Navigation.js
@@ -0,0 +1,12 @@
+import React , {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import {Navbar, Nav} from 'react-bootstrap';
+
+
+export class Navigation extends Component{
+    return(
+        <Navbar bg="dark" expand="lg">
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        </Navbar>
+    )
+}
