7e827e8da0d95d8d58b45d4c4ebdf230b0f05090
Mai Abdelhameed <maiabdelhameed16@gmail.com>
2022-09-22 20:48:39 +0200

    reactapp

---
+++ reactwebapi/src/Department.js
@@ -0,0 +1,9 @@
+import React , {Component} from 'react';
+
+export class Department extends Component{
+    render(){
+        <div className='mt-5 d-flex justify-content-left'>
+            This is department page.
+        </div>
+    }
+}
---
+++ reactwebapi/src/Employee.js
@@ -0,0 +1,9 @@
+import Raact , {Component} from 'react';
+
+export class Employee extends Component{
+    render(){
+        <div className='mt-5 d-flex justify-content-left'>
+            This is Employee page.
+        </div>
+    }
+}
