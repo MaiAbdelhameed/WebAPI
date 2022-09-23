0c353deb9bebed303cfc877f51bced6ebeb3eaf0
Mai Abdelhameed <maiabdelhameed16@gmail.com>
2022-09-24 00:57:15 +0200

    react

--- reactwebapi/src/App.js
+++ reactwebapi/src/App.js
@@ -1,8 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {Home} from './Home';
+import {Employee} from './Employee';
+import {Department} from './Department';
+import {Navigation} from './Navigation';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 
 function App() {
-  return ()
+  return (
+    <BrowserRouter>
+    <div className="container">
+     <h3 className="m-3 d-flex justify-content-center">
+       React JS Tutorial
+     </h3>
+
+     <Navigation/>
+
+     <Switch>
+       <Route path='/' component={Home} exact/>
+       <Route path='/department' component={Department}/>
+       <Route path='/employee' component={Employee}/>
+     </Switch>
+    </div>
+    </BrowserRouter>
   );
 }
 
